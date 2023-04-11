ce2ef6cbf2005053f0821ad616ea77dbd1601423
Asparawr <anbednarczykan@gmail.com>
2023-04-11 03:18:23 +0200

    DBCharacterClass updated

--- DOLDatabase/Tables/DBCharacterClass.cs
+++ DOLDatabase/Tables/DBCharacterClass.cs
@@ -23,15 +23,26 @@ namespace DOL.Database
     [DataTable(TableName = "CharacterClass")]
     public class DBCharacterClass : DataObject
     {
-        private int id;
-        private int specPointMultiplier;
-        private int baseHP;
-        private int baseWeaponSkill;
-        private string autoTrainableSkills;
-        private string eligibleRaces;
+        private byte id;
+        private byte baseClassID;
+        private byte specPointMultiplier;
+        private short baseHP;
+        private short baseWeaponSkill;
+        private string autoTrainableSkills = "";
+        private string eligibleRaces = "";
+        private byte classType;
+        private string name;
+        private string femaleName = "";
+        private string professionTranslationID;
+        private byte primaryStat;
+        private byte secondaryStat;
+        private byte tertiaryStat;
+        private byte manaStat;
+        private bool canUseLeftHandedWeapon;
+        private byte maxPulsingSpells;
 
         [PrimaryKey]
-        public int ID
+        public byte ID
         {
             get => id;
             set
@@ -42,9 +53,53 @@ namespace DOL.Database
         }
 
         [DataElement]
-        public int SpecPointMultiplier
+        public byte BaseClassID
         {
-            get { return specPointMultiplier; }
+            get => baseClassID;
+            set
+            {
+                Dirty = true;
+                baseClassID = value;
+            }
+        }
+
+        [DataElement]
+        public byte ClassType
+        {
+            get => classType;
+            set
+            {
+                Dirty = true;
+                classType = value;
+            }
+        }
+
+        [DataElement]
+        public string Name
+        {
+            get => name;
+            set
+            {
+                Dirty = true;
+                name = value;
+            }
+        }
+
+        [DataElement]
+        public string FemaleName
+        {
+            get => femaleName;
+            set
+            {
+                Dirty = true;
+                femaleName = value;
+            }
+        }
+
+        [DataElement]
+        public byte SpecPointMultiplier
+        {
+            get => specPointMultiplier;
             set
             {
                 Dirty = true;
@@ -53,9 +108,65 @@ namespace DOL.Database
         }
 
         [DataElement]
-        public int BaseHP
+        public string AutoTrainSkills
         {
-            get { return baseHP; }
+            get => autoTrainableSkills;
+            set
+            {
+                Dirty = true;
+                autoTrainableSkills = value;
+            }
+        }
+
+
+        [DataElement]
+        public byte PrimaryStat
+        {
+            get => primaryStat;
+            set
+            {
+                Dirty = true;
+                primaryStat = value;
+            }
+        }
+
+        [DataElement]
+        public byte SecondaryStat
+        {
+            get => secondaryStat;
+            set
+            {
+                Dirty = true;
+                secondaryStat = value;
+            }
+        }
+
+        [DataElement]
+        public byte TertiaryStat
+        {
+            get => tertiaryStat;
+            set
+            {
+                Dirty = true;
+                tertiaryStat = value;
+            }
+        }
+
+        [DataElement]
+        public byte ManaStat
+        {
+            get => manaStat;
+            set
+            {
+                Dirty = true;
+                manaStat = value;
+            }
+        }
+
+        [DataElement]
+        public short BaseHP
+        {
+            get => baseHP;
             set
             {
                 Dirty = true;
@@ -64,9 +175,9 @@ namespace DOL.Database
         }
 
         [DataElement]
-        public int BaseWeaponSkill
+        public short BaseWeaponSkill
         {
-            get { return baseWeaponSkill; }
+            get => baseWeaponSkill;
             set
             {
                 Dirty = true;
@@ -75,24 +186,46 @@ namespace DOL.Database
         }
 
         [DataElement]
-        public string AutoTrainSkills
-        {
-            get { return autoTrainableSkills; }
-            set
-            {
-                Dirty = true;
-                autoTrainableSkills = value;
-            }
-        }
-
-        [DataElement]
         public string EligibleRaces
         {
-            get { return eligibleRaces; }
+            get => eligibleRaces;
             set
             {
                 Dirty = true;
                 eligibleRaces = value;
+            }
+        }
+
+        [DataElement]
+        public bool CanUseLeftHandedWeapon
+        {
+            get => canUseLeftHandedWeapon;
+            set
+            {
+                Dirty = true;
+                canUseLeftHandedWeapon = value;
+            }
+        }
+
+        [DataElement]
+        public string ProfessionTranslationID
+        {
+            get => professionTranslationID;
+            set
+            {
+                Dirty = true;
+                professionTranslationID = value;
+            }
+        }
+
+        //No DataElement on purpose
+        public byte MaxPulsingSpells
+        {
+            get => maxPulsingSpells;
+            set
+            {
+                Dirty = true;
+                maxPulsingSpells = value;
             }
         }
     }
