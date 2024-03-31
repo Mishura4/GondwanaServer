c2b8d177a5750d9f630a9e346bc6d1ea818d4165
Amber Ehrlich <miuna.oshino@gmail.com>
2024-03-30 21:16:33 -0400

    feat: rewrite `/heal` command

--- GameServer/commands/gmcommands/heal.cs
+++ GameServer/commands/gmcommands/heal.cs
@@ -33,15 +33,103 @@ namespace DOL.GS.Commands
         {
             try
             {
-                GameLiving target = client.Player.TargetObject as GameLiving;
+                GameLiving target = client.Player.TargetObject as GameLiving ?? client.Player;
 
-                if (target == null || (args.Length > 1 && args[1].ToLower() == "me"))
-                    target = (GameLiving)client.Player;
+                int argIndex = 1;
+                if (args.Length > argIndex)
+                {
+                    if (args[argIndex] == "me")
+                    {
+                        target = client.Player;
+                        ++argIndex;
+                    }
+                }
 
-                target.Health = target.MaxHealth;
-                target.Endurance = target.MaxEndurance;
-                target.Mana = target.MaxMana;
-            }
+                if (target is not { ObjectState: GameObject.eObjectState.Active })
+                {
+                    client.Player.SendTranslatedMessage("Commands.GM.Heal.BadTarget");
+                    return;
+                }
+
+                if (!target.IsAlive)
+                {
+                    client.Player.SendTranslatedMessage("Commands.GM.Heal.Dead");
+                    return;
+                }
+
+                int value = 100;
+                bool percent = true;
+                if (args.Length > argIndex)
+                {
+                    var arg = args[argIndex];
+                    percent = arg.EndsWith('%');
+
+                    if (int.TryParse(percent ? arg.Substring(0, arg.Length - 1) : arg, out value))
+                    {
+                        if (value < 0)
+                        {
+                            DisplaySyntax(client);
+                            return;
+                        }
+                        argIndex++;
+                    }
+                }
+
+                if (args.Length > argIndex)
+                {
+                    switch (args[argIndex])
+                    {
+                        case "health":
+                            if (percent)
+                            {
+                                target.Health += (int)(target.MaxHealth * (value / 100.0) + 0.5f);
+                                client.Player.SendTranslatedMessage("Commands.GM.Heal.Health.Percent", eChatType.CT_System, eChatLoc.CL_SystemWindow, target.Name, value);
+                            }
+                            else
+                            {
+                                target.Health += value;
+                                client.Player.SendTranslatedMessage("Commands.GM.Heal.Health", eChatType.CT_System, eChatLoc.CL_SystemWindow, target.Name, value);
+                            }
+                            break;
+
+                        case "endu":
+                            if (percent)
+                            {
+                                target.Endurance += (int)(target.MaxEndurance * (value / 100.0) + 0.5f);
+                                client.Player.SendTranslatedMessage("Commands.GM.Heal.Endu.Percent", eChatType.CT_System, eChatLoc.CL_SystemWindow, target.Name, value);
+                            }
+                            else
+                            {
+                                target.Endurance += value;
+                                client.Player.SendTranslatedMessage("Commands.GM.Heal.Endu", eChatType.CT_System, eChatLoc.CL_SystemWindow, target.Name, value);
+                            }
+                            break;
+
+                        case "mana":
+                            if (percent)
+                            {
+                                target.Mana += (int)(target.MaxMana * (value / 100.0) + 0.5f);
+                                client.Player.SendTranslatedMessage("Commands.GM.Heal.Mana.Percent", eChatType.CT_System, eChatLoc.CL_SystemWindow, target.Name, value);
+                            }
+                            else
+                            {
+                                target.Mana += value;
+                                client.Player.SendTranslatedMessage("Commands.GM.Heal.Mana", eChatType.CT_System, eChatLoc.CL_SystemWindow, target.Name, value);
+                            }
+                            break;
+
+                        default:
+                            DisplaySyntax(client);
+                            break;
+                    }
+                    return;
+                }
+
+                target.Health = percent ? (int)(target.MaxHealth * (value / 100.0) + 0.5f) : target.Health + value;
+                target.Endurance = percent ? (int)(target.MaxEndurance * (value / 100.0) + 0.5f) : target.Endurance + value;
+                target.Mana = percent ? (int)(target.MaxMana * (value / 100.0) + 0.5f) : target.Mana + value;
+                client.Player.SendTranslatedMessage(percent ? "Commands.GM.Heal.Percent" : "Commands.GM.Heal", eChatType.CT_System, eChatLoc.CL_SystemWindow, target.Name, value);
+}
             catch (Exception)
             {
                 DisplaySyntax(client);
