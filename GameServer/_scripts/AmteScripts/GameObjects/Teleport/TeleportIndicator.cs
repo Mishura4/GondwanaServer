329be3a1666f7b8b99146d9d8aad1fa777a58919
Amber Ehrlich <miuna.oshino@gmail.com>
2024-07-28 12:27:16 -0400

    fix: add missing TeleportIndicator.cs

---
+++ GameServer/_scripts/AmteScripts/GameObjects/Teleport/TeleportIndicator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DOL.GS.Scripts
+{
+    public class TeleportIndicator : GameNPC
+    {
+        public TeleportNPC Owner { get; set; }
+        
+        public TeleportIndicator(TeleportNPC owner)
+        {
+            Owner = owner;
+        }
+        
+        /// <inheritdoc />
+        public override ushort GetModelForPlayer(GamePlayer player)
+        {
+            return (ushort)(Owner.ShouldShowInvisibleModel(player) ? 1923 : 667);
+        }
+    }
+}
