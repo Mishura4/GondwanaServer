72313a656c05078f7e9b1f59deb7c5562fedacde
Amber Ehrlich <miuna.oshino@gmail.com>
2025-08-24 18:31:05 -0400

    fix: fix SuperFearBrain speed

--- GameServer/spells/negative/SuperFearBrain.cs
+++ GameServer/spells/negative/SuperFearBrain.cs
@@ -25,23 +25,36 @@ namespace DOL.AI.Brain
     public class SuperFearBrain : FearBrain
     {
         short m_maxSpeedBuff = 0;
+        
         ///<summary>
         /// Calculate flee target.
         /// </summary>
         ///<param name="target">The target to flee.</param>
         protected override void CalculateFleeTarget(GameLiving target)
         {
-            var TargetAngle = Body.GetAngleTo(target.Coordinate);
-
-            var fleePoint = Body.Coordinate + Vector.Create(TargetAngle, 300);
+            var targetAngle = Body.Coordinate.GetOrientationTo(target.Coordinate) + Angle.Degrees(180);
+            var speed = (short)(Body.MaxSpeed * 1.3); // 130% speed
+            var fleePoint = Body.Coordinate + Vector.Create(targetAngle, speed * 3); // Flee for 3 seconds
             var point = PathingMgr.Instance.GetClosestPointAsync(Body.CurrentZone, fleePoint, 128, 128, 256);
             Body.StopFollowing();
             Body.StopAttack();
             Body.PathTo(point.HasValue ? Coordinate.Create(point.Value) : fleePoint, Body.MaxSpeed);
-            //set speed to 130%
-            m_maxSpeedBuff = (short)(Body.MaxSpeedBase * 0.3);
-            Body.MaxSpeedBase = (short)(Body.MaxSpeedBase * 1.3);
+            if (Body.Motion.Destination.IsWithinDistance(Body.Motion.CurrentPosition, 0.5, true))
+            {
+                return;
+            }
+            
+            Body.CurrentSpeed = (short)(Body.MaxSpeed * 1.3);
         }
+
+        /// <inheritdoc />
+        protected override void SetBody(GameNPC npc)
+        {
+            base.SetBody(npc);
+        
+            npc.AbilityBonus[eProperty.MaxSpeed] += 30;
+        }
+
         //on removal of the brain set speed to normal
         public override void RemoveEffect()
         {
